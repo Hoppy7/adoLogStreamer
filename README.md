68d2bbab6d1bef9506fd22c6c4a8186db19e5c66
Ross Hopkins <rosshopkins7@gmail.com>
2021-04-29 01:01:28 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# adoLogStreamer
+
+[![Build Status](https://dev.azure.com/hoppy7/Azure/_apis/build/status/Hoppy7.adoLogStreamer?branchName=main)](https://dev.azure.com/hoppy7/Azure/_build/latest?definitionId=10&branchName=main)
---
+++ pipelineRunsTrigger/run.ps1
@@ -0,0 +1,44 @@
+param([string] $QueueItem, $TriggerMetadata)
+
+# get pipeline definition & run ids from message
+$definitionId = $TriggerMetadata["definitionId"];
+$runId = $TriggerMetadata["runId"];
+
+# headers
+$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($env:azureDevOpsPat)"));
+$headers = @{Authorization = "Basic $token"};
+
+# list logs
+try
+{
+    $listLogsUri = "https://dev.azure.com/$($env:azureDevOpsOrg)/$($env:azureDevOpsProject)/_apis/pipelines/$($definitionId)/runs/$($runId)/logs?`$expand=signedContent&api-version=6.0-preview.1";
+    $listResponse = Invoke-WebRequest -Uri $listLogsUri -Method GET -Headers $headers -SkipHttpErrorCheck;
+    $logs = $listResponse.Content | ConvertFrom-Json;
+}
+catch [web.exception]
+{
+    throw "Failed to list the pipeline run logs.  $($_)";
+}
+
+# get logs zip from signedContent url
+try 
+{
+    $zipName = "pipeline_" + $definitionId + "-" + "run_" + $runId + ".zip";
+    $outputPath = "$env:TEMP\$zipName";
+    Invoke-WebRequest -Uri $logs.signedContent.url -Method GET -Headers $headers -SkipHttpErrorCheck -OutFile $outputPath;
+}
+catch [exception]
+{
+    throw "Failed to output the pipeline run's logs zip.  $($_)";
+}
+
+# push to blob
+try
+{
+    $ctx = New-AzStorageContext -ConnectionString $env:AzureWebJobsStorage;
+    Set-AzStorageBlobContent -File $outputPath -Container $env:logOutputContainer -Blob $zipName -Context $ctx -Force;
+}
+catch [exception]
+{
+    throw "Failed to upload pipeline logs zip to blob container. $($_)";
+}
